3415b044d36053f9658305bf26485885a6f6efc1
vlad5667 <vlad.dimitriev1@gmail.com>
2021-05-08 23:32:45 +0300

    #1. Implement knight check.

--- Chess/Chess/Knight.cpp
+++ Chess/Chess/Knight.cpp
@@ -1,4 +1,5 @@
 #include "Knight.h"
+#include "King.h"
 #include <cmath>
 
 namespace ChessGame {
@@ -22,6 +23,71 @@ namespace ChessGame {
 		return false;
 	}
 	int Knight::isCheckOccured(Piece* pieces[32], int(&fields)[8][8]) {
+		int currentZ = this->getZCenter() + 3, currentX = this->getXCenter() + 3;
+		if (currentZ >= 2 && currentX >= 1) {
+			if (fields[currentZ - 2][currentX - 1] != -1) {
+				Piece* p = pieces[fields[currentZ - 2][currentX - 1]];
+				if (typeid(*p) == typeid(King) && p->getColor() != this->getColor()) {
+					return 1;
+				}
+			}
+		}
+		if (currentZ >= 2 && currentX <= 6) {
+			if (fields[currentZ - 2][currentX + 1] != -1) {
+				Piece* p = pieces[fields[currentZ - 2][currentX + 1]];
+				if (typeid(*p) == typeid(King) && p->getColor() != this->getColor()) {
+					return 1;
+				}
+			}
+		}
+		if (currentZ <= 5 && currentX >= 1) {
+			if (fields[currentZ + 2][currentX - 1] != -1) {
+				Piece* p = pieces[fields[currentZ + 2][currentX - 1]];
+				if (typeid(*p) == typeid(King) && p->getColor() != this->getColor()) {
+					return 1;
+				}
+			}
+		}
+		if (currentZ <= 5 && currentX <= 6) {
+			if (fields[currentZ + 2][currentX + 1] != -1) {
+				Piece* p = pieces[fields[currentZ + 2][currentX + 1]];
+				if (typeid(*p) == typeid(King) && p->getColor() != this->getColor()) {
+					return 1;
+				}
+			}
+		}
+		if (currentZ >= 1 && currentX >= 2) {
+			if (fields[currentZ - 1][currentX - 2] != -1) {
+				Piece* p = pieces[fields[currentZ - 1][currentX - 2]];
+				if (typeid(*p) == typeid(King) && p->getColor() != this->getColor()) {
+					return 1;
+				}
+			}
+		}
+		if (currentZ >= 1 && currentX <= 5) {
+			if (fields[currentZ - 1][currentX + 2] != -1) {
+				Piece* p = pieces[fields[currentZ - 1][currentX + 2]];
+				if (typeid(*p) == typeid(King) && p->getColor() != this->getColor()) {
+					return 1;
+				}
+			}
+		}
+		if (currentZ <= 6 && currentX >= 2) {
+			if (fields[currentZ + 1][currentX - 2] != -1) {
+				Piece* p = pieces[fields[currentZ + 1][currentX - 2]];
+				if (typeid(*p) == typeid(King) && p->getColor() != this->getColor()) {
+					return 1;
+				}
+			}
+		}
+		if (currentZ <= 6 && currentX <= 5) {
+			if (fields[currentZ + 1][currentX + 2] != -1) {
+				Piece* p = pieces[fields[currentZ + 1][currentX + 2]];
+				if (typeid(*p) == typeid(King) && p->getColor() != this->getColor()) {
+					return 1;
+				}
+			}
+		}
 		return 0;
 	}
 }
